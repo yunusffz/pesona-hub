127ecb8a310ca0e5f1ddd91ef77d8ced5e7b5540
Yunus Ahmad Baktir <yunus.baktir@gmail.com>
2025-11-20 08:59:40 +0700

    refactor: replace PNG images with AVIF format in RegisterForm component for improved performance and adjust layout properties

--- app/components/features/register/RegisterForm.vue
+++ app/components/features/register/RegisterForm.vue
@@ -1,9 +1,9 @@
 <template>
   <section
-    class="relative grid grid-cols-1 lg:grid-cols-2 min-h-screen w-full 2xl:max-w-[1536px] 2xl:mx-auto bg-white overflow-hidden"
+    class="relative grid grid-cols-1 lg:grid-cols-2 h-screen w-full 2xl:max-w-[1536px] 2xl:mx-auto bg-white overflow-hidden"
   >
-    <NuxtImg
-      src="/assets/images/login.png"
+    <img
+      src="/assets/images/login.avif"
       alt="Register Image"
       class="lg:hidden absolute inset-0 object-cover w-full h-full z-10"
       loading="lazy"
@@ -13,8 +13,8 @@
       <div class="absolute inset-0 z-10 register-gradient-overlay"></div>
 
       <!-- Background image -->
-      <NuxtImg
-        src="/assets/images/login.png"
+      <img
+        src="/assets/images/login.avif"
         alt="Register Image"
         class="absolute inset-0 object-cover w-full h-full"
       />
@@ -29,10 +29,10 @@
 
     <!-- FORM SECTION -->
     <div
-      class="relative z-20 flex items-center justify-center p-6 sm:p-8 xl:p-12 lg:bg-white"
+      class="relative z-20 flex items-center justify-center p-6 sm:p-8 xl:p-12 lg:bg-white overflow-y-auto"
     >
       <div
-        class="w-full max-w-[340px] sm:max-w-[380px] lg:max-w-[440px] bg-white lg:bg-transparent rounded-2xl lg:rounded-none shadow-2xl lg:shadow-none p-6 sm:p-8 lg:p-0 lg:px-2 max-h-screen overflow-y-auto lg:py-12"
+        class="w-full max-w-[340px] sm:max-w-[380px] lg:max-w-[440px] bg-white lg:bg-transparent rounded-2xl lg:rounded-none shadow-2xl lg:shadow-none p-6 sm:p-8 lg:p-0 lg:px-2 max-h-screen lg:py-12"
       >
         <div class="flex flex-col gap-4 sm:gap-6 lg:gap-8">
           <!-- Header -->

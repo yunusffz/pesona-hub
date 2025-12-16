0ef14fcbcdf7133662591523e94c28aef5e56dc1
Yunus <yunus.baktir@gmail.com>
2025-12-16 16:14:57 +0700

    feat: update CTASection component to enhance messaging and add WhatsApp contact options; include new WhatsApp SVG icon asset

--- app/components/common/cta-section/CTASection.vue
+++ app/components/common/cta-section/CTASection.vue
@@ -11,30 +11,51 @@
           Mari tumbuh bersama untuk masa depan yang hijau.
         </h1>
         <p class="mt-6 text-md text-[#585858]">
-          Bergabunglah dengan kami dalam membangun ekonomi hijau yang
-          berkelanjutan.
+          Kami percaya pertumbuhan berkelanjutan dimulai dari kolaborasi. Pesona
+          Hub Digi siap terhubung dengan Anda — dari pelaku usaha, penggerak
+          komunitas, hingga mitra yang peduli pada keberlanjutan alam Indonesia.
         </p>
 
         <div class="mt-[42px]">
           <NuxtLink to="mailto:pupspskl.klhk@gmail.com" target="_blank">
-            <BaseButton>Kontak Kami</BaseButton>
+            <BaseButton>Hubungi Kami</BaseButton>
           </NuxtLink>
+        </div>
+        <div class="mt-10">
+          <div>Atau hubungi whatsapp kami :</div>
+          <div class="flex gap-3 mt-[14px]">
+            <NuxtLink
+              to="https://wa.me/6281907540184"
+              target="_blank"
+              class="flex items-center text-primary gap-1 hover:underline"
+            >
+              <SvgIcon name="whatsapp" class="text-primary" /> +62 819-0754-0184
+            </NuxtLink>
+            <NuxtLink
+              to="https://wa.me/6281380063303"
+              target="_blank"
+              class="flex items-center text-primary gap-1 hover:underline"
+            >
+              <SvgIcon name="whatsapp" class="text-primary" /> +62 813-8006-3303
+            </NuxtLink>
+          </div>
         </div>
       </div>
     </div>
   </section>
 </template>
 <script setup lang="ts">
-  import BaseButton from "~/components/base/BaseButton.vue";
-  import IconBadge from "~/components/base/IconBadge.vue";
+import BaseButton from "~/components/base/BaseButton.vue";
+import IconBadge from "~/components/base/IconBadge.vue";
+import SvgIcon from "~/components/base/SvgIcon.vue";
 </script>
 <style scoped>
-  .background-cta {
-    background: linear-gradient(
-      285.94deg,
-      #eecbaa 0%,
-      #ffff7e 49.88%,
-      #eecbaa 100%
-    );
-  }
+.background-cta {
+  background: linear-gradient(
+    285.94deg,
+    #eecbaa 0%,
+    #ffff7e 49.88%,
+    #eecbaa 100%
+  );
+}
 </style>
